0553cebd96767b7b02f102bfe266ad795068e156
AtumaKen <atumaken@gmail.com>
2021-04-29 10:54:52 +0100

    completed login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,16 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:haggle_x/widgets/background_widget.dart';
 import 'package:haggle_x/widgets/textfield_widget.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool _obscureState = true;
+
+  _changeObscureState() {
+    if (mounted)
+      setState(() {
+        _obscureState = !_obscureState;
+      });
+  }
   @override
   Widget build(BuildContext context) {
+    //todo: add validation to textfields
     return Scaffold(
       body: BackgroundWidget(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 29.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+          child: ListView(
             children: [
+              SizedBox(
+                height: 120,
+              ),
               Container(
                   alignment: Alignment.centerLeft,
                   child: Text(
@@ -19,13 +35,71 @@ class LoginScreen extends StatelessWidget {
                     style: TextStyle(
                         color: Theme.of(context).primaryColor,
                         fontSize: 40,
-                        fontWeight: FontWeight.bold),
+                        fontWeight: FontWeight.w500),
                   )),
-              SizedBox(height: 40,),
-              TextFieldWidget(labelText: "Email Address",),
-              TextFieldWidget(labelText: "Password (Min. 8 characters)",),
-              Text("Forgot Password?"),
-              ElevatedButton(onPressed: () {}, child: Text("LOG IN"))
+              SizedBox(
+                height: 40,
+              ),
+              TextFieldWidget(
+                labelText: "Email Address",
+                obscureText: false,
+              ),
+              SizedBox(
+                height: 17,
+              ),
+              TextFieldWidget(
+                obscureText: _obscureState,
+                suffix: GestureDetector(
+                  child: Icon(
+                    Icons.remove_red_eye_rounded,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                  onTap: _changeObscureState,
+                ),
+                labelText: "Password (Min. 8 characters)",
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                  child: Text(
+                    "Forgot Password?",
+                    style: TextStyle(
+                        fontSize: 11, color: Theme.of(context).primaryColor, fontWeight: FontWeight.w300),
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "LOG IN",
+                    style: TextStyle(
+                      color: Color.fromRGBO(62, 6, 6, 1),
+                    ),
+                  ),
+                  style: TextButton.styleFrom(
+                      backgroundColor: Theme.of(context).accentColor),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+               TextButton(
+                child: Text(
+                  "New user?Create a new account",
+                  style: TextStyle(
+                      fontSize: 11, color: Theme.of(context).primaryColor, fontWeight: FontWeight.w300),
+                ),
+                onPressed: () {},
+              ),
             ],
           ),
         ),
--- lib/widgets/textfield_widget.dart
+++ lib/widgets/textfield_widget.dart
@@ -1,20 +1,30 @@
 import 'package:flutter/material.dart';
 
 class TextFieldWidget extends StatelessWidget {
-  final String? labelText;
+  final String labelText;
+  final bool obscureText;
+  final Widget? suffix;
 
-  TextFieldWidget({this.labelText});
+  TextFieldWidget(
+      {Key? key,
+      required this.labelText,
+      required this.obscureText,
+      this.suffix});
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       cursorColor: Theme.of(context).primaryColor,
+      obscureText: obscureText,
       style: TextStyle(color: Theme.of(context).primaryColor),
       decoration: InputDecoration(
+        suffix: suffix,
         focusColor: Color.fromRGBO(160, 133, 225, 1),
         labelText: labelText,
-        labelStyle:
-            TextStyle(color: Theme.of(context).primaryColor, fontSize: 12, fontWeight: FontWeight.w100),
+        labelStyle: TextStyle(
+            color: Theme.of(context).primaryColor,
+            fontSize: 11,
+            fontWeight: FontWeight.w100),
         enabledBorder: UnderlineInputBorder(
           borderSide: BorderSide(color: Theme.of(context).primaryColor),
         ),
